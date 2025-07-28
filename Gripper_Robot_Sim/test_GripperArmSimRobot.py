d72259ef2ea474b610c141649d73748874dbfdf9
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-07-28 20:54:45 +0000

    Jul 28, 2025, 10:54 PM

--- Gripper_Robot_Sim/test_GripperArmSimRobot.py
+++ Gripper_Robot_Sim/test_GripperArmSimRobot.py
@@ -143,28 +143,34 @@ new_pose = Pose3d_gripper(translation_left=new_translation_left,
                           rotation_left=pose.rotation_left, 
                           rotation_right=pose.rotation_left) #Create a new Pose3d with same orientation but new position
 ik_solution = robot.inverse_kinematics(new_pose)
-force = 1 #lowering the speed to prevent the block from falling
+force = 0.1 #lowering the speed to prevent the block from falling
 robot.set_target_effector_pose(new_pose,force)
 for _ in range(100):
     p.stepSimulation()
     time.sleep(1 / 240.0)
     
 
-# plaing the block to another place
-target_center = np.array([0.35, 0, 0.0])
-offset = np.array([0.03, 0, 0])  # assume fingers are 6cm apart
-new_translation_left = target_center - offset
-new_translation_right = target_center + offset
-new_pose = Pose3d_gripper(translation_left=new_translation_left,
-                          translation_right=new_translation_right,
-                          rotation_left=pose.rotation_left, 
-                          rotation_right=pose.rotation_left) #Create a new Pose3d with same orientation but new position
-ik_solution = robot.inverse_kinematics(new_pose)
-force = 1 #lowering the speed to prevent the block from falling
-robot.set_target_effector_pose(new_pose,force)
-for _ in range(100):
+closing_width =  0.5
+robot.set_the_fingers_open_close(closing_width)
+for _ in range(50):
     p.stepSimulation()
     time.sleep(1 / 240.0)
+
+# # plaing the block to another place
+# target_center = np.array([1, 1, 0.0])
+# offset = np.array([0.03, 0, 0])  # assume fingers are 6cm apart
+# new_translation_left = target_center - offset
+# new_translation_right = target_center + offset
+# new_pose = Pose3d_gripper(translation_left=new_translation_left,
+#                           translation_right=new_translation_right,
+#                           rotation_left=pose.rotation_left, 
+#                           rotation_right=pose.rotation_left) #Create a new Pose3d with same orientation but new position
+# ik_solution = robot.inverse_kinematics(new_pose)
+# force = 1 #lowering the speed to prevent the block from falling
+# robot.set_target_effector_pose(new_pose,force)
+# for _ in range(100):
+#     p.stepSimulation()
+#     time.sleep(1 / 240.0)
 
     
 print("___________________testing if the location of end effector==the point it want to go_____________")
