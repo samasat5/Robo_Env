51bef90854374e00417662da9692278f9f153a0d
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-07-28 19:49:14 +0000

    Jul 28, 2025, 9:49 PM

--- Gripper_Robot_Sim/test_GripperArmSimRobot.py
+++ Gripper_Robot_Sim/test_GripperArmSimRobot.py
@@ -5,7 +5,6 @@ Testing GripperArmSimRobot : an Franka Panda robot with a gripper (two fingers)
 
 """
 
-
 import pybullet as p
 import pybullet_data
 import time
@@ -118,7 +117,7 @@ for _ in range(50):
     
 
 # moving the block to another place
-target_center = np.array([0.4, -0.3, 0.01 + 0.15])
+target_center = np.array([0.4, 0.4, 0.01 + 0.15])
 offset = np.array([0.03, 0, 0])  # assume fingers are 6cm apart
 new_translation_left = target_center - offset
 new_translation_right = target_center + offset
