0be44b96bf13f28844505554a0d67cf107af13f3
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-07-28 20:04:50 +0000

    Jul 28, 2025, 10:04 PM

--- Gripper_Robot_Sim/test_GripperArmSimRobot.py
+++ Gripper_Robot_Sim/test_GripperArmSimRobot.py
@@ -45,6 +45,7 @@ block = p.loadURDF("block2.urdf",[0.2, 0.5, 0.01]  , useFixedBase = False )
 workspace = p.loadURDF("workspace.urdf",[0.35, 0, 0.0]  , useFixedBase = False )
 zone = p.loadURDF("zone.urdf",[0.35, 0, 0.0]  , useFixedBase = False )
 
+
 # Create robot with end effector
 robot = GripperArmSimRobot(pybullet_client=p)
 
