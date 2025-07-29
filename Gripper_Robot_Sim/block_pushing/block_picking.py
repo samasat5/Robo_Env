5df378a293fe48bad4306816bb7438509d11fa70
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-07-29 08:31:23 +0000

    Jul 29, 2025, 10:31 AM

--- Gripper_Robot_Sim/block_pushing/block_picking.py
+++ Gripper_Robot_Sim/block_pushing/block_picking.py
@@ -367,9 +367,11 @@ class BlockPush(gym.Env):
         if reset_poses:
             self._pybullet_client.restoreState(self._saved_state)
 
-            rotation = transform.Rotation.from_rotvec([0, math.pi, 0])
-            translation = np.array([0.3, -0.4, self.effector_height])
-            starting_pose = Pose3d(rotation=rotation, translation=translation)
+            rotation_left = transform.Rotation.from_rotvec([0, math.pi, 0])
+            rotation_right = transform.Rotation.from_rotvec([0, math.pi, 0])
+            translation_left = np.array([0.3, -0.4, self.effector_height])
+            translation_right = np.array([0.3, -0.4, self.effector_height])
+            starting_pose = Pose3d_gripper(rotation_left=rotation_left,rotation_right=rotation_right, translation_left=translation_left,translation_right=translation_right)
             self._set_robot_target_effector_pose(starting_pose)
 
             # Reset block pose.
