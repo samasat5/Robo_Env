f8d6eb96ac001b1a78bfd90d7436995ab402fc23
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-07-30 09:47:58 +0000

    Jul 30, 2025, 11:47 AM

--- Gripper_Robot_Sim/block_pushing/block_picking2.py
+++ Gripper_Robot_Sim/block_pushing/block_picking2.py
@@ -585,12 +585,9 @@ class BlockPick(gym.Env):
                 )
             )
 
-        state["targets"] = []
-        if self._target_ids:
-            for target_id in self._target_ids:
-                state["targets"].append(
-                    ObjState.get_bullet_state(self._pybullet_client, target_id)
-)
+  
+        state["target"] = ObjState.get_bullet_state(self._pybullet_client, self._target_id)
+
 
         state["objects"] = []
         for obj_id in self.get_obj_ids():
