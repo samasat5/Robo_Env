2af6fe9ce8439049f590ca8b982f11c539844f46
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-08-01 18:20:38 +0000

    Aug 1, 2025, 8:20 PM

--- Gripper_Robot_Sim/block_pushing/block_picking3.py
+++ Gripper_Robot_Sim/block_pushing/block_picking3.py
@@ -295,16 +295,19 @@ class BlockPick(gym.Env):
         self._pybullet_client.restoreState(self._saved_state)
         
         # Reset the _target_pose
-        # (Where the robot is trying to move to) :
+        # (The pose the robot is trying to reach to) :
         orientation_left = transform.Rotation.from_rotvec([0, math.pi, 0])
         orientation_right = transform.Rotation.from_rotvec([0, math.pi, 0])
         target_center = np.array([0.3, -0.4, self.effector_height])
         new_translation_left = target_center - self.offset
         new_translation_right = target_center + self.offset
-        
         starting_pose = Pose3d_gripper(translation_left=new_translation_left,
                                 translation_right=new_translation_right,
                                 orientation_left=orientation_left, 
                                 orientation_right=orientation_right) 
         self._set_robot_target_effector_pose(starting_pose)
+        
+        # Reset the block pose: 
+        
+
         
