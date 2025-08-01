66d591a0b05cbe201771593523bbeb4341f0b2c5
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-08-01 17:19:56 +0000

    Aug 1, 2025, 7:19 PM

--- Gripper_Robot_Sim/test_GripperArmSimRobot2.py
+++ Gripper_Robot_Sim/test_GripperArmSimRobot2.py
@@ -167,7 +167,7 @@ _workspace_uid = utils_pybullet.load_urdf(
     WORKSPACE_URDF_PATH,
     basePosition=[0.35, 0, 0.0],)
 
-# _robot = franka_panda_sim_robot.GripperArmSimRobot(p,INITIAL_JOINT_POSITIONS)
+_robot = franka_panda_sim_robot.GripperArmSimRobot(p,INITIAL_JOINT_POSITIONS)
 
 
 
@@ -386,48 +386,6 @@ for _ in range(100):
 
 """ compute state
 """
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-robot_id  = p.loadURDF("franka_panda/panda.urdf",[0,0,0], [0,0,0,1], useFixedBase = True )
-p.resetDebugVisualizerCamera(
-    cameraDistance=1.5,
-    cameraYaw=90,
-    cameraPitch=-40,
-    cameraTargetPosition=[0, 0, 0.1],
-)
-# information on the joints
-movable_joints = []
-for i in range(p.getNumJoints(robot_id)):
-    joint_info = p.getJointInfo(robot_id, i)
-    joint_type = joint_info[2]
-    joint_name = joint_info[1].decode("utf-8")
-    if joint_type in [p.JOINT_REVOLUTE, p.JOINT_PRISMATIC]: # only the joints that move 
-        movable_joints.append(i)
-        p.addUserDebugText(
-            f"{i}: {joint_name}",
-            [0, 0, 0.1],
-            parentObjectUniqueId=robot_id,
-            parentLinkIndex=i,
-            textColorRGB=[1, 0, 0],
-            textSize=1.0,)
-
-# move the joints one by one
-for joint_index in movable_joints:
-    print(f"Moving joint {joint_index}")
-    for t in range(240):  
-        angle = 0.5 * math.sin(t*0.05) # using sin to have a oscillating movement 
-        p.setJointMotorControl2(
-            robot_id,
-            joint_index,
-            controlMode=p.POSITION_CONTROL,
-            targetPosition=angle,
-            force=400,)
-        p.stepSimulation()
-        time.sleep(1)
-
-
-while True:
-    p.stepSimulation()
-    time.sleep(1)
     
 block_position = p.getBasePositionAndOrientation(_block_id)[0]
 block_orientation = p.getBasePositionAndOrientation(_block_id)[1]
---
+++ Gripper_Robot_Sim/test_GripperArmSimRobot3.py
@@ -0,0 +1,50 @@
+
+import pybullet as p
+import pybullet_data
+import time
+import numpy as np
+import pdb
+import math
+p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+robot_id  = p.loadURDF("franka_panda/panda.urdf",[0,0,0], [0,0,0,1], useFixedBase = True )
+p.resetDebugVisualizerCamera(
+    cameraDistance=1.5,
+    cameraYaw=90,
+    cameraPitch=-40,
+    cameraTargetPosition=[0, 0, 0.1],
+)
+# information on the joints
+movable_joints = []
+for i in range(p.getNumJoints(robot_id)):
+    joint_info = p.getJointInfo(robot_id, i)
+    joint_type = joint_info[2]
+    joint_name = joint_info[1].decode("utf-8")
+    if joint_type in [p.JOINT_REVOLUTE, p.JOINT_PRISMATIC]: # only the joints that move 
+        movable_joints.append(i)
+        p.addUserDebugText(
+            f"{i}: {joint_name}",
+            [0, 0, 0.1],
+            parentObjectUniqueId=robot_id,
+            parentLinkIndex=i,
+            textColorRGB=[1, 0, 0],
+            textSize=1.0,)
+
+# move the joints one by one
+for joint_index in movable_joints:
+    print(f"Moving joint {joint_index}")
+    for t in range(240):  
+        angle = 0.5 * math.sin(t*0.05) # using sin to have a oscillating movement 
+        p.setJointMotorControl2(
+            robot_id,
+            joint_index,
+            controlMode=p.POSITION_CONTROL,
+            targetPosition=angle,
+            force=400,)
+        p.stepSimulation()
+        time.sleep(1)
+
+
+while True:
+    p.stepSimulation()
+    time.sleep(1)
