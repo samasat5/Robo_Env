ae190ee4d67bdf825457c717413a187cb77ae9d1
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-08-01 16:33:54 +0000

    Aug 1, 2025, 6:33 PM

--- Gripper_Robot_Sim/test_GripperArmSimRobot2.py
+++ Gripper_Robot_Sim/test_GripperArmSimRobot2.py
@@ -460,18 +460,18 @@ obs = collections.OrderedDict(
 
 
 print("block_translation=",
-      block_pose.translation[0:2])
+      block_pose.translation[0:3])
 print("block_orientation=",
       _yaw_from_pose(block_pose))
 
 print("gripper_translation_left=",
-      robot_pose.translation_left[0:2])
+      robot_pose.translation_left[0:3])
 print("gripper_translation_right=",
-      robot_pose.translation_right[0:2])
+      robot_pose.translation_right[0:3])
 print("gripper_orientation_left=",
-      robot_yaw_from_pose(block_pose[0]))
+      gripper_orientation_left)
 print("gripper_orientation_right=",
-      robot_yaw_from_pose(block_pose[1]))
+      gripper_orientation_right)
 
 print("target_translation=",
       _target_pose.translation[0:2])
