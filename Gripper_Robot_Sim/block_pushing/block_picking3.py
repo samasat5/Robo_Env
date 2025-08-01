9f1ff6c0629598f740a332ffbc2aba7572085e42
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-08-01 10:51:47 +0000

    Aug 1, 2025, 12:51 PM

--- Gripper_Robot_Sim/block_pushing/block_picking3.py
+++ Gripper_Robot_Sim/block_pushing/block_picking3.py
@@ -265,8 +265,8 @@ class BlockPick(gym.Env):
         obs = collections.OrderedDict(
             block_translation=block_pose.translation[0:2],
             block_orientation=_yaw_from_pose(block_pose),
-            gripper_translation_left=robot_pose.translation_left,
-            gripper_translation_right=robot_pose.translation_right,
+            gripper_translation_left=robot_pose.translation_left[0:2],
+            gripper_translation_right=robot_pose.translation_right[0:2],
             effector_target_translation=self._target_effector_pose.translation[0:2],
             target_translation=self._target_pose.translation[0:2],
             target_orientation=_yaw_from_pose(self._target_pose),
