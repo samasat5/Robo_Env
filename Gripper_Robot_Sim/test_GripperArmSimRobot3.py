90af60f780bef26f55c61cd36436fa8a0ccfd5d8
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-08-02 16:31:58 +0000

    Aug 2, 2025, 6:31 PM

--- Gripper_Robot_Sim/test_GripperArmSimRobot3.py
+++ Gripper_Robot_Sim/test_GripperArmSimRobot3.py
@@ -73,7 +73,7 @@ robot.move_gripper_to_target(block_position,force)
 for _ in range(100):
     p.stepSimulation()
     time.sleep(1 / 240.0)
-closing_width = -0.0001 
+closing_width = 0.001
 force = 4
 robot.set_the_fingers_open_close(closing_width,force)
 for _ in range(100):
