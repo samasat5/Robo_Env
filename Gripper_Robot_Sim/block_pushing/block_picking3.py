8aae216fd496f9712a7893d87944e7bc276b7fb4
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-08-02 08:42:10 +0000

    Aug 2, 2025, 10:42 AM

--- Gripper_Robot_Sim/block_pushing/block_picking3.py
+++ Gripper_Robot_Sim/block_pushing/block_picking3.py
@@ -391,14 +391,8 @@ class BlockPick(gym.Env):
         else: 
             self._is_grasped = False
     
+    
     def _compute_goal_distance(self, state):
         goal_translation = self.get_goal_translation()
-        if self._task != BlockTaskVariant.REACH:
-            goal_distance = np.linalg.norm(
-                state["block_translation"] - goal_translation[0:2]
-            )
-        else:
-            goal_distance = np.linalg.norm(
-                state["effector_translation"] - goal_translation[0:2]
-            )
+        goal_distance = np.linalg.norm(state["effector_translation"] - goal_translation[0:2])
         return goal_distance
