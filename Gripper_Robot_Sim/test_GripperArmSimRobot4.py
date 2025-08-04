a472e1ca2843da15974a98bda9b6af3239b2e651
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-08-04 23:25:22 +0000

    Aug 5, 2025, 1:25 AM

--- Gripper_Robot_Sim/test_GripperArmSimRobot4.py
+++ Gripper_Robot_Sim/test_GripperArmSimRobot4.py
@@ -5,9 +5,9 @@ import numpy as np
 import gym
 import matplotlib.pyplot as plt
 from block_pushing.utils.franka_panda_sim_robot import GripperArmSimRobot
+import block_pushing.block_picking3
 
-
-env = gym.make("BlockInsert-v0")
+env = gym.make("BlockPick-v0")
 
 obs = env.reset()
 done = False
