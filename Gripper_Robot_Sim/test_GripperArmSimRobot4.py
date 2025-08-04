f85285d5d22a72543119acc7abd05815b81f5694
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-08-04 23:23:30 +0000

    Aug 5, 2025, 1:23 AM

---
+++ Gripper_Robot_Sim/test_GripperArmSimRobot4.py
@@ -0,0 +1,31 @@
+import gym
+import time
+import numpy as np
+
+import gym
+import matplotlib.pyplot as plt
+from block_pushing.utils.franka_panda_sim_robot import GripperArmSimRobot
+
+
+env = gym.make("BlockInsert-v0")
+
+obs = env.reset()
+done = False
+step = 0
+
+for i in range(10):
+    action = env.action_space.sample()  # random action
+    # action = dataset[i]['action'].squeeze(0).numpy()
+    obs, reward, done, info = env.step(action)
+
+    print(f"Step {step}: reward={reward:.3f}, done={done}")
+    print("Event info:", info)
+
+    # Optional: render as RGB
+    img = env.render(mode="rgb_array")
+    plt.imshow(img)
+    plt.title(f"Step {step}")
+    plt.pause(0.1)
+    step += 1
+
+env.close()
