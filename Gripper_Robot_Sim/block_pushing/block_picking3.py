db1b10ef440dee1c0973b537bb9ed504da0ffa38
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-08-06 12:25:36 +0000

    Aug 6, 2025, 2:25 PM

--- Gripper_Robot_Sim/block_pushing/block_picking3.py
+++ Gripper_Robot_Sim/block_pushing/block_picking3.py
@@ -141,6 +141,7 @@ class BlockPick(gym.Env):
 
         
     
+    
     def _setup_the_scene(self):
         
         # bullet_client.BulletClient(connection_mode=self._connection_mode)
@@ -318,6 +319,38 @@ class BlockPick(gym.Env):
         # if self._image_size is not None:
         #     obs["rgb"] = self.show_camera_img(self._image_size)
         return obs
+    
+    def _create_observation_space(self, image_size):
+        pi2 = math.pi * 2
+
+        obs_dict = collections.OrderedDict(
+            block_translation=spaces.Box(low=-5, high=5, shape=(3,)),  # x,y
+            
+            block_orientation=spaces.Box(low=-pi2, high=pi2, shape=(1,)),  # phi
+            
+            effector_translation=spaces.Box(
+                low=self.workspace_bounds[0] - 0.1,  # Small buffer for to IK noise.
+                high=self.workspace_bounds[1] + 0.1,
+            ),  # x,y
+            
+            effector_target_translation=spaces.Box(
+                low=self.workspace_bounds[0] - 0.1,  # Small buffer for to IK noise.
+                high=self.workspace_bounds[1] + 0.1,
+            ),  # x,y
+            
+            target_translation=spaces.Box(low=-5, high=5, shape=(3,)),  # x,y
+            
+            target_orientation=spaces.Box(
+                low=-pi2,
+                high=pi2,
+                shape=(1,),
+            ),  # theta
+        )
+        if image_size is not None:
+            obs_dict["rgb"] = spaces.Box(
+                low=0, high=255, shape=(image_size[0], image_size[1], 3), dtype=np.uint8
+            )
+        return spaces.Dict(obs_dict)
     
     def reset(self,reset_poses= True):
         self._pybullet_client.restoreState(self._saved_state)
