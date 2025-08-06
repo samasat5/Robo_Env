53f3192561653cb26194070e66d673701a0e7afa
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-08-06 10:46:03 +0000

    Aug 6, 2025, 12:46 PM

--- Gripper_Robot_Sim/block_pushing/block_picking3.py
+++ Gripper_Robot_Sim/block_pushing/block_picking3.py
@@ -84,10 +84,14 @@ class BlockPick(gym.Env):
             self._camera_instrinsics = CAMERA_INTRINSICS_REAL
             self._workspace_urdf_path = WORKSPACE_URDF_PATH_REAL
         
-        self._connection_mode = pybullet.GUI
+        # self._connection_mode = pybullet.DIRECT
         self._pybullet_client = bullet_client.BulletClient(connection_mode=self._connection_mode)
         if shared_memory:
-            self._connection_mode = pybullet.SHARED_MEMORY
+            connection_mode = pybullet.SHARED_MEMORY
+        else: 
+            connection_mode = pybullet.GUI
+        self._connection_mode = connection_mode
+        self._pybullet_client = bullet_client.BulletClient(connection_mode=self._connection_mode)
         self._setup_the_scene()
         self._image_size = image_size
         self.image_size = np.array([320, 240])
