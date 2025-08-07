c9e19402d8ad2020bf09a4d9996b536e9fdbb9f6
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-08-07 13:42:37 +0000

    Aug 7, 2025, 3:42 PM

--- Gripper_Robot_Sim/block_pushing/block_picking3.py
+++ Gripper_Robot_Sim/block_pushing/block_picking3.py
@@ -487,12 +487,12 @@ class BlockPick(gym.Env):
         if np.allclose(move_to_position, target_block_pos):
             target_block_pos = np.array(p_state["block_translation"])
             # twist_amount = p_state["block_orientation"][0]*1.5
-            twist_amount = 1
+            twist_amount = 2
             
             print("\n\n\n\n")
-            print("self._robot.get_joint_state(6)",self._robot.get_joint_state(6))
+            print("before twist :_robot.get_joint_state(6)",self._robot.get_joint_state(6))
             self._robot.set_target_pick_the_block(target_block_pos,twist_amount)
-            print("self._robot.get_joint_state(6)",self._robot.get_joint_state(6))
+            print("after twist: _robot.get_joint_state(6)",self._robot.get_joint_state(6))
             print("\n\n\n\n")
             
             state = self._compute_state()
