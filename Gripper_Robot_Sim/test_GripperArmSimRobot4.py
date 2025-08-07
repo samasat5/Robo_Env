b29f90b31d980c98ab544e165f3e0d0753d564e0
Sama <Sama.Satariyan@etu.sorbonne-universite.fr>
2025-08-07 10:19:49 +0000

    Aug 7, 2025, 12:19 PM

--- Gripper_Robot_Sim/test_GripperArmSimRobot4.py
+++ Gripper_Robot_Sim/test_GripperArmSimRobot4.py
@@ -1,48 +1,68 @@
 import gym
 import time
 import numpy as np
-
+import pybullet as p
 import gym
 import matplotlib.pyplot as plt
 from block_pushing.utils.franka_panda_sim_robot import GripperArmSimRobot
 from block_pushing.block_picking3 import BlockPick
+from block_pushing.utils.franka_panda_sim_robot import GripperArmSimRobot
 import block_pushing.block_picking3
 
 
-# env = gym.make("BlockPick-v0")
-env = BlockPick(image_size=None)
-
-# obs = env.reset(seed=42)
-obs = env.reset()
-done = False
-step = 0
-
-trajectory = []
-z_height = env.effector_height 
-
-block_pos = obs['block_translation']
-target_pos = obs['target_translation']
-actions = [
-    np.r_[block_pos[:2], env.effector_height],  
-    np.r_[target_pos[:2], env.effector_height],  
-]
 
 
-for i, action in enumerate(actions):
-    # action = env.action_space.sample()  # random action
-    # action = dataset[i]['action'].squeeze(0).numpy()
-    obs, reward, done, info = env.step(action)
+INITIAL_JOINT_POSITIONS = np.array(
+    [
+        0.0, 
+        -0.5235987755982988, 
+        0.0, 
+        -1.0471975511965976, 
+        0.0, 
+        1.5707963267948966, 
+        0.0, 
+        0.0, 
+        0.0
+    ])
+GripperArmSimRobot(  
+            p,
+            initial_joint_positions=INITIAL_JOINT_POSITIONS, )
 
-    frame = env.render() if hasattr(env, 'render') else None
 
-    trajectory.append({
-        "step": i,
-        "obs": obs,
-        "action": action.tolist(),
-        "reward": reward,
-        "done": done,
-        "info": info,
-        "frame": frame  # optional, remove if not needed
-    })
+# # env = gym.make("BlockPick-v0")
+# env = BlockPick(image_size=None)
 
-env.close()
+# # obs = env.reset(seed=42)
+# obs = env.reset()
+# done = False
+# step = 0
+
+# trajectory = []
+# z_height = env.effector_height 
+
+# block_pos = obs['block_translation']
+# target_pos = obs['target_translation']
+# actions = [
+#     np.r_[block_pos[:2], env.effector_height],  
+#     np.r_[target_pos[:2], env.effector_height],  
+# ]
+
+
+# for i, action in enumerate(actions):
+#     # action = env.action_space.sample()  # random action
+#     # action = dataset[i]['action'].squeeze(0).numpy()
+#     obs, reward, done, info = env.step(action)
+
+#     frame = env.render() if hasattr(env, 'render') else None
+
+#     trajectory.append({
+#         "step": i,
+#         "obs": obs,
+#         "action": action.tolist(),
+#         "reward": reward,
+#         "done": done,
+#         "info": info,
+#         "frame": frame  # optional, remove if not needed
+#     })
+
+# env.close()
